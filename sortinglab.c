dab2a98f8bee8d0598e5ae87084e5f4baad491ba
alyssapinnock <alyssanpinnock@gmail.com>
2024-03-07 23:00:52 -0500

    functions and arrays listed

--- sortinglab.c
+++ sortinglab.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void bubbleSort();
+
+void selectionSort();
+
+void numSwaps();
+
 int main(
+    int array1[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    int array2[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
 
 
 
